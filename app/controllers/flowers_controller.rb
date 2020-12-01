161c2dd1e2b98add4d3bdd886b6d6b7ee9dbeef0
ayaayato <73512438+ayaayato@users.noreply.github.com>
2020-12-01 13:18:50 +0900

    コントローラー設定

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -4,6 +4,22 @@ def index
 end
 
 def new
+  @flower = Flower.new
+end
+
+def create
+  @flower = Flower.new(flower_params)
+ if @flower.save
+  redirect_to root_path
+ else
+ render :new
+ end
+end
+
+private
+
+def flower_params
+  params.require(:item).permit(:title, :place, :image, :category_id, :subcategory_id).merge(user_id: current_user.id)
 end
 
 end
