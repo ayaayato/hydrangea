4506ee74ca08eef1703c46bb1df2c8c4a5e81069
ayaayato <73512438+ayaayato@users.noreply.github.com>
2020-12-01 15:10:05 +0900

    編集機能の実装

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,4 +1,7 @@
 class FlowersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :create, :destroy]
+  before_action :find_flower, only: [:show, :edit, :update, :destroy]
+  before_action :exile_to_index, only: [:edit, :destroy]
 
 def index
   @flowers = Flower.all.order("created_at DESC")
@@ -18,13 +21,35 @@ def create
 end
 
 def show
-  @flower = Flower.find(params[:id])
+end
+
+def edit
+end
+
+def update
+  @flower.update(flower_params)
+if @flower.save
+   redirect_to flower_path(@flower.id)
+else
+ render :edit
+end
 end
 
 private
 
 def flower_params
   params.require(:flower).permit(:title, :place, :image, :category_id, :subcategory_id).merge(user_id: current_user.id)
+end
+
+def find_flower
+  @flower = Flower.find(params[:id])
+end
+
+def exile_to_index
+  @flower = Flower.find_by(id:params[:id])
+   unless @flower.user_id == current_user.id
+     redirect_to action: :index
+   end
 end
 
 end
