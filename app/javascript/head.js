c017c2176ed080007ef575e5b9d55d3f15dda8db
ayaayato <73512438+ayaayato@users.noreply.github.com>
2020-12-05 14:53:59 +0900

    バルーン表示の追加

--- app/javascript/head.js
+++ app/javascript/head.js
@@ -13,13 +13,72 @@ $(function(){
         height: '-=10'
       },300)
     });
+    //$(".icons").hover(
+      //function(){
+        //$(this).fadeTo(300, 0.7);
+      //},
+      //function(){
+        //$(this).fadeTo(300, 1);
+      //}
+      //);
+
+
+    $(".new-btn").hover(
+      function(){
+        $("#new-balloon").fadeIn();
+      },
+      function(){
+        $("#new-balloon").fadeOut();
+      });
+    
+    $(".seach-btn").hover(
+      function(){
+        $("#seach-balloon").fadeIn();
+      },
+      function(){
+        $("#seach-balloon").fadeOut();
+      });  
+
+    $(".user-btn").hover(
+      function(){
+        $("#user-balloon").fadeIn();
+      },
+      function(){
+        $("#user-balloon").fadeOut();
+      });
+
+    $(".coin-btn").hover(
+      function(){
+        $("#coin-balloon").fadeIn();
+      },
+      function(){
+        $("#coin-balloon").fadeOut();
+      });
+
+    $(".fav-flowerbtn").hover(
+      function(){
+        $("#fav-flower-balloon").fadeIn();
+      },
+      function(){
+        $("#fav-flower-balloon").fadeOut();
+      });
+      
+    $(".fav-userbtn").hover(
+      function(){
+        $("#fav-user-balloon").fadeIn();
+      },
+      function(){
+        $("#fav-user-balloon").fadeOut();
+      });
 
     $(".blue-icon").hover(
       function(){
         $(this).fadeTo(300, 0.7);
+        $("#index-balloon").fadeIn();
       },
       function(){
         $(this).fadeTo(300, 1);
+        $("#index-balloon").fadeOut();
       }
       );
 
